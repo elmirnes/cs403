0bb2e107e460faf91a503731a1b34b4ccc81fa02
elmirnes <nesirlielmir09@gmail.com>
2024-05-25 06:39:04 +0400

    feat modify

--- Homework/task_2/script.js
+++ Homework/task_2/script.js
@@ -269,60 +269,60 @@
 //})
 //console.log(countIndex);
 ///////////// TASK 12//////////////
-let arr2 = [
-    {
-        name: 'test',
-        key: 1
-    },
-    {
-        name: 'task',
-        key: 2
-    },
-    {
-        name: 'tanqo',
-        key: 3
-    },
-    {
-        name: 'like',
-        key: 4
-    },
-    {
-        name: 'task',
-        key: 5
-    },
-    {
-        name: 'trust',
-        key: 6
-    },
-    {
-        name: 'test',
-        key: 7
-    },
-    {
-        name: 'last',
-        key: 8
-    },
-    {
-        name: 'tanqo',
-        key: 9
-    },
-    {
-        name: 'elephant',
-        key: 10
-    },
-    {
-        name: 'love',
-        key: 11
-    },
-    {
-        name: 'small',
-        key: 12
-    },
-    {
-        name: 'little',
-        key: 13
-    },
-]
+//let arr2 = [
+//    {
+//        name: 'test',
+//        key: 1
+//    },
+//    {
+//        name: 'task',
+//        key: 2
+//    },
+//    {
+//        name: 'tanqo',
+//        key: 3
+//    },
+//    {
+//        name: 'like',
+//        key: 4
+//    },
+//    {
+//        name: 'task',
+//        key: 5
+//    },
+//    {
+//        name: 'trust',
+//        key: 6
+//    },
+//    {
+//        name: 'test',
+//        key: 7
+//    },
+//    {
+//        name: 'last',
+//        key: 8
+//    },
+//    {
+//        name: 'tanqo',
+//        key: 9
+//    },
+//    {
+//        name: 'elephant',
+//        key: 10
+//    },
+//    {
+//        name: 'love',
+//        key: 11
+//    },
+//    {
+//        name: 'small',
+//        key: 12
+//    },
+//    {
+//        name: 'little',
+//        key: 13
+//    },
+//]
 //QEYD Bu tasklarda arr2 istifade edilecekdir
 // 16 arr2 de "name"-i "t" herfi ile bashlayan obyektleri yeni arraya yigin
 //newArr2=[]
